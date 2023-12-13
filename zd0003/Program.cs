a0040a8bbd150a4076f178e050efb259cac0815b
Eva Bespalova <evysia6@gmail.com>
2023-12-13 20:24:51 +0500

    third task

---
+++ zd0003/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача 3: Задайте произвольную строку. Выясните,
+//является ли она палиндромом.
+
+//“aBcD1ef!-” => Нет
+//“шалаш” => Да
+//“55655” => Да
+
+Console.WriteLine("Введите любое слово");
+string text = IsEmptyText(Console.ReadLine());
+char [] characters = text.ToCharArray();
+Console.WriteLine("is polindrome? " + IsPollindrome(characters));
+
+bool IsPollindrome (char [] chars)
+{
+    int count = 0;
+    for (int i = 0; i < chars.Length/2; i++)
+    {
+        if(chars[i] == chars[chars.Length - 1 - i])
+        {
+            count++;
+        }
+    }
+    if (count == chars.Length/2) return true;
+    else return false;
+}
+
+string IsEmptyText(string str) //проверка на пустой ввод
+{
+    if(str == string.Empty)
+    {
+        Console.WriteLine("Вы не ввели слово, попробуйте снова");
+        return IsEmptyText(Console.ReadLine());
+    }
+    else return str;
+}

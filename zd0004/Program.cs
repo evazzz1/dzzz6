545d95d0536e64120b212cd08ccad8f585d4ab93
Eva Bespalova <evysia6@gmail.com>
2023-12-13 22:17:16 +0500

    other task

---
+++ zd0004/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую
+// из слов, разделенных пробелами. Сформировать строку,
+// в которой слова расположены в обратном порядке. В
+// полученной строке слова должны быть также разделены
+// пробелами.
+
+// “Hello my world” => “world my Hello”
+
+string text = "Hello my world";
+char [] letters = text.ToCharArray();
+string result = GetText(text);
+Console.WriteLine(result);
+
+string GetText (string str)
+{
+    string[] words = str.Split(' ');
+    Array.Reverse(words);
+    return string.Join(" ", words);
+}
